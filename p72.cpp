9b990e477244cd0de558c74756859af4d03ac5bc
Mbina <117299384+mobinaqabeigii@users.noreply.github.com>
2022-11-27 21:48:12 +0330

    Add files via upload

---
+++ p72.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	int m,n;
+	int i=0;
+	while(m>=n){
+		i+=1;
+		m-=n;
+	}
+	cout<<"maghsom aleih";
+	cout<<i;
+	cout<<"baghimande";
+	cout<<m;
+	
+	
+	
+}
